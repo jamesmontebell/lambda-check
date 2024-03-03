37c5d44c1836595055f918f73bfe69208b2c3b96
James Montebell <100632392+jamesmontebell@users.noreply.github.com>
2024-03-03 01:26:04 -0500

    api done

--- serve.py
+++ serve.py
@@ -1,11 +1,31 @@
 from fastapi import FastAPI
+import sqlite3
 
 app = FastAPI()
+conn = sqlite3.connect('haskVul.db')
 
-@app.get("/search")
-def read_root():
-    return {"Hello": "World"}
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, query_param: str = None):
-    return {"item_id": item_id, "query_param": query_param}
+@app.get("/search/")
+async def search_items(term: str = None):
+    if term:
+        cursor = conn.cursor()
+        query = f"SELECT summary, Severity FROM packageVul WHERE cve_description LIKE '%{term}%'"
+        cursor.execute(query)
+        conn.commit()
+        results = cursor.fetchone()
+        cursor.close()
+        if results:
+            results = list(results)
+            final = []
+            final.append(results[0].replace("\n"," ").replace("#", ""))
+            final.append(results[1].replace("\n"," ").replace("#", ""))
+            return final[0] + ' - ' + final[1]
+        else:
+            return "No vulnerabilities found"
+    else:
+        return {"message": "No search query provided"}
+    
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
