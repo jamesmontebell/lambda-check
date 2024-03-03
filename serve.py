b029d952a74545a87441695101dc58e724f27899
darkfutrue <bpb9629@gmail.com>
2024-03-03 00:11:40 -0500

    Serve.py is now birthed

---
+++ serve.py
@@ -0,0 +1,11 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/sear")
+def read_root():
+    return {"Hello": "World"}
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, query_param: str = None):
+    return {"item_id": item_id, "query_param": query_param}
