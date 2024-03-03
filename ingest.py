676f4871e065f252a7689694c8943b6246d7273a
James Montebell <100632392+jamesmontebell@users.noreply.github.com>
2024-03-02 22:04:27 -0500

    json formatting

--- ingest.py
+++ ingest.py
@@ -1,6 +1,7 @@
 from dotenv import load_dotenv
 import os
 import requests
+import sqlite3
 
 load_dotenv()
 
@@ -15,14 +16,15 @@ def get_github(id):
     headers={'Authorization': 'token ' + api_key}
   )
   if response.status_code == 200:
-    data = response.text
+    data = response.json()
     return data
   else:
     print('Error:', response.status_code, response.reason)
   
 
+github_list = []
 for id in ghsa_id:
-  print(get_github(id))
+  github_list.append(get_github(id))
 
 def get_nvd():
   response = requests.get(
@@ -30,9 +32,48 @@ def get_nvd():
   )
 
   if response.status_code == 200:
-    return response.text
+    return response.json()
   else:
     print('Error:', response.status_code, response.reason)\
 
-print(get_nvd())
+nvd = get_nvd()
 
+github_dict = {}
+count = 0
+for cve in github_list:
+  github_dict[str(count)] = {
+    "cve_id" : cve["cve_id"],
+    "severity" : cve["severity"],
+    "html_url" : cve["html_url"],
+    "summary" : cve["summary"],
+    "description" : cve["description"],
+  }
+  for v in cve["vulnerabilities"]:
+    github_dict[str(count)]["package_name"] = v["package"]["name"]
+    github_dict[str(count)]["version"] = v["vulnerable_version_range"]
+  count += 1
+
+# print(github_dict)
+
+nvd_dict = {}
+count = 0
+for cve in nvd["vulnerabilities"]:
+  nvd_dict[str(count)] = {
+    "cve_id" : cve["cve"]["id"],
+  }
+  for x in cve["cve"]["descriptions"]:
+      if x["lang"] == "en":
+        nvd_dict[str(count)]["description"] = x["value"]
+      break
+    
+  for d in cve["cve"]["metrics"]["cvssMetricV31"]:
+    nvd_dict[str(count)]["severity"] = d["cvssData"]["baseSeverity"]
+    nvd_dict[str(count)]["version"] = d["cvssData"]["version"]
+  
+  
+  nvd_dict[str(count)]["package_name"] = "haskell package vulnerability"
+  nvd_dict[str(count)]["html_url"] = "https://nvd.nist.gov/vuln/search/results?form_type=Basic&results_type=overview&query=haskell&search_type=all&isCpeNameSearch=false"
+  nvd_dict[str(count)]["summary"] = nvd_dict[str(count)]["description"][:45] + "..."
+  count += 1
+
+print(nvd_dict)
