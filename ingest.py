de6a9258662fabda445a9887b74df6acc7ec4dd0
James Montebell <100632392+jamesmontebell@users.noreply.github.com>
2024-03-02 19:57:52 -0500

    ready for sqlite

--- ingest.py
+++ ingest.py
@@ -9,34 +9,30 @@ nvd_id = os.getenv("NVD_ID")
 
 ghsa_id = ['GHSA-75rw-34q6-72cr', 'GHSA-r5mv-3cq3-727q', 'GHSA-mgm6-c25g-557g', 'GHSA-w6w5-x32x-cg2w', 'GHSA-4q83-8359-v5v3']#Biscuit
 
-my_dict = {}
-
-for value in ghsa_id:
+def get_github(id):
   response = requests.get(
-    f'https://api.github.com/advisories/{value}',
+    f'https://api.github.com/advisories/{id}',
     headers={'Authorization': 'token ' + api_key}
   )
   if response.status_code == 200:
-    data = response.json()
-    # print(data)
-    my_dict[value] = data
+    data = response.text
+    return data
   else:
     print('Error:', response.status_code, response.reason)
+  
 
-for key, value in my_dict.items():
-  for k, v in value.items():
-    print(k)
-  break
+for id in ghsa_id:
+  print(get_github(id))
 
-nvd_data = None
-
-response = requests.get(
+def get_nvd():
+  response = requests.get(
   url= 'https://services.nvd.nist.gov/rest/json/cves/2.0?keywordSearch=haskell',
-)
+  )
 
-if response.status_code == 200:
-  nvd_data = response.json()
-else:
-  print('Error:', response.status_code, response.reason)
+  if response.status_code == 200:
+    return response.text
+  else:
+    print('Error:', response.status_code, response.reason)\
 
-print(nvd_data)
+print(get_nvd())
+
