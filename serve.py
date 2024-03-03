fd6beddcaf6a31ac144c4f9b7bb8ec756c6990e8
Isaac Dugan <102197130+idugan100@users.noreply.github.com>
2024-03-03 07:26:58 -0500

    Update serve.py

--- serve.py
+++ serve.py
@@ -28,4 +28,4 @@ async def search_items(term: str = None):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8005)
+    uvicorn.run(app, host="localhost", port=8005)
